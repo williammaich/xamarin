e2975fbd1d7c6abfdb3f5a782c3226d60d3a3de0
Williammaich <williammaich@hotmail.com>
2017-06-11 14:40:41 -0300

    feito o fim da aula 2 e item taped

--- App2/App2/MainPage.xaml.cs
+++ App2/App2/MainPage.xaml.cs
@@ -7,6 +7,10 @@ namespace App2
    public class Veiculo{
         public string Nome { get; set; }
         public decimal Preco { get; set; }
+        public string PrecoFormatado
+        {
+            get { return string.Format("R$ {0}", Preco); }
+        }
     }
 
     public partial class MainPage : ContentPage
@@ -28,7 +32,20 @@ namespace App2
             this.BindingContext = this;
             
 		}
-	}
+
+        private void listViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            //esse metodo foi criado no xaml para um evento de toque.
+
+
+            var veiculo = (Veiculo )e.Item;
+            // coloca a variavel () e tambem converte o objeto para veiculo colocando entre parenteses.
+            //e. é o objeto que ja vem instanciado.
+
+            DisplayAlert("Test Drive", string.Format("você tocou no modelo '{0}', que custa {1}", veiculo.Nome, veiculo.PrecoFormatado), "ok");
+            //aqui faz o alerta e tras a mensagem quando clicar {0} é o marcador do list tras o nome   {1} tras o preco
+        }
+    }
 
    
 }
