f65a719d6309d4a21935033e47477208e8be4fe2
Williammaich <williammaich@hotmail.com>
2017-06-11 13:08:52 -0300

    primeiro commit de xamarin

---
+++ App2/App2.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace App2.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new App2.App());
+        }
+    }
+}
---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App2
+{
+
+   public class Veiculo{
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+
+    public partial class MainPage : ContentPage
+	{
+        public System.Collections.Generic.List<Veiculo> Veiculos { get; set; }
+
+
+        public MainPage()
+		{
+			InitializeComponent();
+
+            this.Veiculos = new System.Collections.Generic.List<Veiculo>
+            {
+                new Veiculo {Nome = "Azera V6", Preco = 60000},
+                new Veiculo {Nome = "Fiesta 2.0", Preco = 50000},
+                new Veiculo {Nome = "HB20 S", Preco = 40000}
+            };
+
+            this.BindingContext = this;
+            
+		}
+	}
+
+   
+}
