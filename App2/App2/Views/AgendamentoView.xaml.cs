a41ac526a34771e891ec45986dd52cefe1612fb2
Williammaich <williammaich@hotmail.com>
2017-06-13 23:45:48 -0300

    feito a terceira tela

--- App2/App2/Views/AgendamentoView.xaml.cs
+++ App2/App2/Views/AgendamentoView.xaml.cs
@@ -14,6 +14,39 @@ namespace App2.Views
 	{
         public Veiculo Veiculo { get; set; }
 
+        public string Nome { get; set; }
+        public string Fone  { get; set; }
+        public string Email { get; set; }
+
+
+        DateTime dataAgendamento = DateTime.Today;
+        public DateTime DataAgendamento {
+            get
+            {
+                return dataAgendamento;
+            }
+              set
+            {
+                dataAgendamento = value;
+            }
+
+        }
+
+
+        TimeSpan horaAgendamento = TimeSpan.Zero;
+        public TimeSpan HoraAgendamento {
+            get
+            {
+                return horaAgendamento;
+            }
+            set {
+                horaAgendamento = value;
+            }
+                }
+
+
+
+
         public AgendamentoView (Veiculo veiculo)
 		{
            
@@ -22,5 +55,12 @@ namespace App2.Views
             this.Veiculo = veiculo;
             this.BindingContext = this;
 		}
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Agendamento", "Nome: " + Nome, "OK");
+        }
+
 	}
 }
--- App2/App2/Views/DetalheView.xaml.cs
+++ App2/App2/Views/DetalheView.xaml.cs
@@ -12,8 +12,90 @@ namespace App2.Views
 	
 	public partial class DetalheView : ContentPage
 	{
+
+        private const int FREIO_ABS = 800;
+        private const int AR_CONDICIONADO = 1000;
+        private const int MP3_PLAYER = 500;
+
+
+
         public Veiculo Veiculo { get; set; }
-        
+
+        public string TextoFreioABS {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0}", FREIO_ABS);
+            }
+        }
+
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R$ {0}", AR_CONDICIONADO);
+            }
+        }
+
+        public string TextoMP3Player
+        {
+
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", MP3_PLAYER);
+            }
+
+        }
+
+        bool temFreioABS;
+        public bool TemFreioABS
+        {
+            get { return temFreioABS; }
+            set
+            {
+                temFreioABS = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+                
+            }
+        }
+
+        bool temArCondicionado;
+        public bool TemArCondicionado
+        {
+            get { return temArCondicionado; }
+            set
+            {
+                temArCondicionado = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+
+            }
+        }
+
+        bool temMP3Player;
+        public bool TemMP3Player
+        {
+            get { return temMP3Player; }
+            set
+            {
+                temMP3Player = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(ValorTotal));
+
+            }
+        }
+
+        public string ValorTotal {
+            get
+            {
+                return string.Format("Valor Total: R$ {0}", Veiculo.Preco +  (TemFreioABS ? FREIO_ABS: 0) + (TemArCondicionado ? AR_CONDICIONADO : 0) + (TemMP3Player ? MP3_PLAYER : 0));
+            }
+        }
+
+
 		public DetalheView (Veiculo veiculo)
 		{
 			InitializeComponent ();
