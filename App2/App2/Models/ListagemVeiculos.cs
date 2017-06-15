1f33c076f0b24e8587f5aad46da1ee54d58dacb6
Williammaich <williammaich@hotmail.com>
2017-06-15 00:35:59 -0300

    criação das models

---
+++ App2/App2/Models/ListagemVeiculos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Models
+{
+    class ListagemVeiculos
+    {
+    }
+}
---
+++ App2/App2/Models/Veiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2.Models
+{
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string PrecoFormatado
+        {
+            get { return string.Format("R$ {0}", Preco); }
+        }
+    }
+}
